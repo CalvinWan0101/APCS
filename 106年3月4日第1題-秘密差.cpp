894503882942051e317ed1f0351c8a6010efe8f9
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-17 15:22:06 +0800

    Add files via upload

---
+++ 106年3月4日第1題-秘密差.cpp
@@ -0,0 +1,27 @@
+/*#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//6╓юда
+	int a, b;
+	string c;
+	int array[1001];
+	while (cin >> c)
+	{
+		a = 0; b = 0;
+		for (int i = 0; i < c.length(); i++)
+		{
+			array[i] = c[i];
+			array[i] -= 48;
+		}
+		for (int i = 0; i < c.length(); i += 2)
+			a += array[i];
+		for (int i = 1; i < c.length(); i += 2)
+			b += array[i];
+		if (a > b)
+			cout << a - b << endl;
+		else
+			cout << b - a << endl;
+	}
+}*/
